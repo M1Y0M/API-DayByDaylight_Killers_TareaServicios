376d3bdadfa26c2c2be26125fbbca69bae843943
Javier <javprezpend@gmail.com>
2026-01-13 22:21:40 +0000

    Primer commit

---
+++ src/main/java/com/apiservicios/dbdkillers/ApiDaybydaylightkillersApplication.java
@@ -0,0 +1,13 @@
+package com.apiservicios.dbdkillers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDaybydaylightkillersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDaybydaylightkillersApplication.class, args);
+	}
+
+}
